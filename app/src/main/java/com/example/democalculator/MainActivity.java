c1e9967aba9565ab3a18b74ecbb9303880a4a6e6
pratheek <pratheekmanimaran@gmail.com>
2022-10-30 12:13:55 +0530

    changes made to MainActivity.java, on click C output space is set to nothing like input.

--- app/src/main/java/com/example/democalculator/MainActivity.java
+++ app/src/main/java/com/example/democalculator/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
     {
         data.setText("");
         workings = "";
-        data.setText("");
+        output.setText("");
         leftBracket = true;
     }
 
